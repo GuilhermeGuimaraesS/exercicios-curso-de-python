bbebfe1787b3a337bfb9610e2a4aff128aae1954
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-23 09:36:50 -0300

    Update ex105.2.py

--- PythonExercises/Mundo 3/ex105.2.py
+++ PythonExercises/Mundo 3/ex105.2.py
@@ -1,6 +1,13 @@
 # Solução realizada pelo professor Guanabara
 
 def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param num: uma ou mais notas dos alunos (aceita várias).
+    :param situ: valor opcional, indicando se deve ou não adicionar a situação.
+    :return: dicionário com várias informações sobre a situação da turma da turma.
+    """
+
     r = dict()
     r['total'] = len(n)
     r['maior'] = max(n)
