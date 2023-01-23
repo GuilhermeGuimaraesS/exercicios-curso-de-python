db6ef7193482ce79f49a1658be59a33cdf8521aa
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-23 09:47:03 -0300

    Update ex106.2.py

--- PythonExercises/Mundo 3/ex106.2.py
+++ PythonExercises/Mundo 3/ex106.2.py
@@ -1,2 +1,20 @@
 # Solução criada pelo professor Gustavo Guanabara
+def ajuda(com):
+    help(com)
 
+def título(msg, cor=0):
+    tam = len(msg)
+    print('~' * tam)
+    print(msg)
+    print('~' * tam)
+
+
+# programa principal
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP')
+    comando = str(input("Função ou Biblioteca > "))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
