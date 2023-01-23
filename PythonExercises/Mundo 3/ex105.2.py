d6f76db0d49eedba42aaeb8f4839f6129f2c0373
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-23 09:33:44 -0300

    Update ex105.2.py

--- PythonExercises/Mundo 3/ex105.2.py
+++ PythonExercises/Mundo 3/ex105.2.py
@@ -6,7 +6,13 @@ def notas(*n, sit=False):
     r['maior'] = max(n)
     r['menor'] = min(n)
     r['media'] = (sum(n)/len(n))
-
+    if sit:
+        if r['media'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['media'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
     return r
 
 # programa principal
