3a637db624000ca7943f0fb3a8c717457af42a7b
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-23 10:05:31 -0300

    Update ex106.2.py

--- PythonExercises/Mundo 3/ex106.2.py
+++ PythonExercises/Mundo 3/ex106.2.py
@@ -1,20 +1,36 @@
 # Solução criada pelo professor Gustavo Guanabara
+c = ('\033[m', # 0 - sem cores
+     '\033[0;30;41m', # 1 - vermelho 
+     '\033[0;30;42m', # 2 - verde 
+     '\033[0;30;43m', # 3 - amarelo 
+     '\033[0;30;44m', # 4 - azul 
+     '\033[0;30;45m', # 5 - roxo 
+     '\033[7;30m' # 6 - branco 
+    );
+
+
 def ajuda(com):
+    título(f'ACESSANDO O MANUAL DO COMANDO \'{com}\'', 4)
+    print(c[6], end='')
     help(com)
+    print(c[0], end='')
 
 def título(msg, cor=0):
-    tam = len(msg)
+    tam = len(msg) + 4
+    print(c[cor], end='')
     print('~' * tam)
-    print(msg)
+    print( f' {msg}' )
     print('~' * tam)
+    print(c[0], end='')
 
 
 # programa principal
 comando = ''
 while True:
-    título('SISTEMA DE AJUDA PyHELP')
+    título('SISTEMA DE AJUDA PyHELP', 2)
     comando = str(input("Função ou Biblioteca > "))
     if comando.upper() == 'FIM':
         break
     else:
         ajuda(comando)
+título('ATÉ LOGO!', 1)
