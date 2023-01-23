1c12a3b00bae5901871426a5d06e3efaa3fa0be5
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-23 19:08:11 -0300

    resolução ex108 e ex109

---
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex108/moeda.py
@@ -0,0 +1,62 @@
+def aumentar(n = 0, p = 0):
+    """
+    ->  Aumenta o valor recebido do usuário de acordo com o percentual desejado.
+    :param n: valor que será mudado.
+    :param p: percentual de aumento.
+    :return: valor com o aumento.
+    Função criada por Guilherme Guimarães dos Santos
+    """
+    alter = n * (p / 100)
+    res = n + alter
+    return res
+
+
+def diminuir(n = 0, p = 0):
+    """
+    ->  Diminue o valor recebido do usuário de acordo com o percentual desejado.
+    :param n: valor que será mudado.
+    :param p: percentual de redução.
+    :return: valor com a redução
+    Função criada por Guilherme Guimarães dos Santos
+    """
+    alter = n * (p / 100)
+    res = n - alter
+    return res
+
+
+def dobro(n = 0):
+    """
+    -> Calcula o dobro do valor fornecido
+    :param n: valor que será calculado.
+    :return: dobro do valor fornecido.
+    Função criada por Guilherme Guimarães dos Santos
+    """
+    res = n * 2
+    return res
+
+
+def metade(n = 0):
+    """
+    -> Calcula a metade do valor fornecido
+    :param n: valor que será calculado.
+    :return: metade do valor fornecido.
+    Função criada por Guilherme Guimarães dos Santos
+    """
+    res = n / 2
+    return res
+
+
+def moeda(n = 0):
+    """
+    -> Converte o valor fornecido para um valor monetário
+    :param n: valor que será convertido.
+    :return: valor alterado e com R$.
+    Função criada por Guilherme Guimarães dos Santos
+    """
+    n = f'{n:,.2f}' # Deixa o valor com apenas duas casas decimais
+    res = n.replace('.', ',') # Troca ponto por vírgula
+    return f'R${res}'
+
+# RESOLUÇÃO DO GUANABARA
+# def moeda(n = 0, moeda='R$'):
+#     return f'{moeda}{n:>.2f}'.replace('.',',')
