912951fc912070ab2fd0a577f42615c73e7fe10b
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-24 11:18:21 -0300

    resolução ex113

---
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex113/__init__.py
@@ -0,0 +1,5 @@
+from autenticador import leiaFloat, leiaInt
+
+numero_int = leiaInt('Digite um número inteiro: ')
+numero_real = leiaFloat('Digite um número real: ')
+print(f'O valor inteiro digitado foi {numero_int} e o valor real digitado foi {numero_real} ')
---
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex113/autenticador.py
@@ -0,0 +1,30 @@
+def leiaInt(msg=''):
+    """
+    -> Função que faz a leitura apenas de valores inteiros. Caso não seja um valor inteiro, retorna uma mensagem de erro e tentar fazer a leitura novamente
+    :param msg: Recebe uma mensagem para ser exibida para o usuário
+    :return: valor inteiro
+    """
+    while True:
+        try:
+            num = int(input(msg))
+        except:
+            print('\033[0;31mERRO! Digite um número inteiro válido \033[m')
+        else:
+            break
+    return num
+
+
+def leiaFloat(msg=''):
+    """
+    -> Função que faz a leitura apenas de valores reais. Caso não seja um valor real, retorna uma mensagem de erro e tentar fazer a leitura novamente
+    :param msg: Recebe uma mensagem para ser exibida para o usuário
+    :return: valor real
+    """
+    while True:
+        try:
+            num = float(input(msg))
+        except:
+            print('\033[0;31mERRO! Digite um valor real válido \033[m')
+        else:
+            break
+    return num
