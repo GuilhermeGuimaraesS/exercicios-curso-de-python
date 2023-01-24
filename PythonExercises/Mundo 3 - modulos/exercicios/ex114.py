8e4fed6c28b47c61e196812b6d6c84611e319134
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-24 12:13:57 -0300

    Update ex114.py

--- PythonExercises/Mundo 3 - modulos/exercicios/ex114.py
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex114.py
@@ -1,6 +1,9 @@
 import urllib.request
 
 try:
-
+    site_pudim = urllib.request.urlopen('http://pudim.com.br/')
+    leitura = site_pudim.read()
 except:
-    print('O site ')
+    print('\033[0;31mO site Pudim não está acessível! \033[m')
+else:
+    print('\033[0;32mO site Pudim está acessível! \033[m')
