e37930972ebb3d46cef356765f827ad3a7063fd9
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-25 10:43:38 -0300

    função leiaNome() ok

--- PythonExercises/Mundo 3 - modulos/exercicios/ex115/cadastro.py
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex115/cadastro.py
@@ -2,34 +2,41 @@
 
     # - Nome; Criar uma função que leia apenas nomes. A função irá validar se foi digitado apenas strings
 
-def vazio(entrada, msg):
+def vazio(entr, msg):
     while True:
-        if entrada == '':
-            print('ERRO! Digite um nome válido... ')
-            entrada = str(input(msg)).strip().upper()
+        if entr == '':
+            print('\033[0;31mERRO! Digite um nome válido... \033[m')
+            entr = str(input(msg)).strip().upper()
         else:
+            n = entr
             break
-    return entrada
+    return n
+
     
 def leiaNome(msg):
     valido = False
-    name = ''
+    # name = ''
     while not valido:
         entrada = str(input(msg)).strip().upper()
-        nome_sobrenome = vazio(entrada, msg)
-        nome_sobrenome = entrada.split() # Fatia a entrada, dividindo nome e sobrenome
+        nome_sobrenome = vazio(entrada, 'Nome ') 
+        nome_sobrenome_f = nome_sobrenome.split() # Fatia a entrada, dividindo nome e sobrenome
 
         pontos_validos = 0
-        for cont in range(0, len(nome_sobrenome)): # Verifica se todas as partes da entrada são letras
-            if nome_sobrenome[cont].isalpha():
+        for cont in range(0, len(nome_sobrenome_f)): # Verifica se todas as partes da entrada são letras
+            if nome_sobrenome_f[cont].isalpha():
                 pontos_validos += 1
-        if pontos_validos == len(nome_sobrenome):
+        if pontos_validos == len(nome_sobrenome_f):
             valido = True
-            name = entrada
+            name_full = nome_sobrenome
         else:
-            print('ERRO! Digite um nome válido... ')
-    return name
+            print('\033[0;31mERRO! Digite um nome válido... \033[m')
+    return name_full
 
+
+# def leiaIdade(msg):
+#     age = 0
+#     try:
+#         age = int(input(msg))
 
 cliente = leiaNome('Nome:  ')
 print(cliente)
