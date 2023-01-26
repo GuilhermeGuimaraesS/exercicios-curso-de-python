99824031610f03b01c525931b6598d9625feb472
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2023-01-26 11:43:18 -0300

    update 2 ex115
    Funções opçao() e operaçao() criadas e otimização no código principal

--- PythonExercises/Mundo 3 - modulos/exercicios/ex115/autenticador.py
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex115/autenticador.py
@@ -15,7 +15,8 @@ def vazio(entr, msg):
             break
     return n
 
-    
+
+# - Nome; Criar uma função que leia apenas nomes. A função irá validar se foi digitado apenas letras.
 def leiaNome(msg):
     """
     -> Faz a leitura apenas de nomes.
@@ -41,6 +42,7 @@ def leiaNome(msg):
     return name_full
 
 
+# - Idade; Criar uma função que leia apenas números inteiros. A função irá validar se foi digitado apenas números
 def leiaIdade(msg):
     """
     -> Função para ler a idade do(a) cadastrado(a). Lê apenas valores inteiros
@@ -67,7 +69,6 @@ def titulo(msg):
 
 class NumeroForaDoIntervalo(Exception): # Gera classe de erro de intervalo
     pass
-
 def intervalo(n, i, f):
     """
     -> Função para verificar se a variável analisada está no intervalo desejado
@@ -82,12 +83,61 @@ def intervalo(n, i, f):
     else:
         return n 
     
-    
+
+# Criar menu principal do sistema e fazer a coleta dos dados
 def menu(lista):
+    """
+    -> Função que recebe uma lista com títulos e retorna um menu personalizado com as opções e os números que correspondem àquela opção.
+    :lista: lista com as funcionalidades
+    """
     titulo('MENU PRINCIPAL') 
     c = 1
     for item in lista:
         print(f'\033[0;33m{c}\033[m - \033[0;34m{item}\033[m')  
         c += 1
     print('-' * 50)
+    
+
+def opçao():
+    """
+    -> Função que faz a leitura e validação da escolha. Verifica se foi digitado um valor inteiro entre 1 e 3
+    :return: opção digitada pelo usuário
+    """
+    while True:
+        try:
+            escolha = int(input('\033[0;33mOpção desejada: \033[m'))
+            intervalo(escolha, 1, 4)
+        except NumeroForaDoIntervalo:
+            print('\033[0;31mOpção inválida! Digite um número entre 1 e 3...\033[m')
+        except:
+            print('\033[0;31mOpção inválida! Digite um número entre 1 e 3...\033[m')  
+        else:
+            break
+    return escolha
+
+        
+def operaçao(num):
+    from time import sleep
+    # Bloco que lista as pessoas cadastradas
+    if num == 1:
+        sleep(1)
+        titulo('PESSOAS CADASTRADAS')
+        with open("C://Users/guilh/Documents/GitHub/exercicios-curso-de-python/PythonExercises/Mundo 3 - modulos/exercicios/ex115/Cadastro.txt", "r") as arquivo:
+            ficha = arquivo.read()
+        print(ficha)
+        sleep(1)
+        
+    # Bloco que realiza um novo cadastro
+    if num == 2:
+        sleep(1)
+        titulo('NOVO CADASTRO')
+        nome = leiaNome('Nome: ')
+        idade = leiaIdade('Idade: ')
+        # Guarda as informações digitadas em um arquivo .txt; Caso não exista, será criado na hora
+        with open("C://Users/guilh/Documents/GitHub/exercicios-curso-de-python/PythonExercises/Mundo 3 - modulos/exercicios/ex115/Cadastro.txt", "a") as arquivo: # Arquivo == variável que recebe o .txt
+            arquivo.write(f'\n{nome.ljust(40)} {idade} anos')    
+        print(f'Novo registro de {nome} adicionado. ')
+        sleep(1)
+    
+
 
--- PythonExercises/Mundo 3 - modulos/exercicios/ex115/cadastro.py
+++ PythonExercises/Mundo 3 - modulos/exercicios/ex115/cadastro.py
@@ -1,57 +1,18 @@
 # Cadastrar pessoas com nome e idade
-from autenticador import NumeroForaDoIntervalo
-from autenticador import  intervalo
 from time import sleep
+from autenticador import menu, opçao, titulo, operaçao
 
-# - Nome; Criar uma função que leia apenas nomes. A função irá validar se foi digitado apenas letras.
-from autenticador import leiaNome
 
-# - Idade; Criar uma função que leia apenas números inteiros. A função irá validar se foi digitado apenas números
-from autenticador import leiaIdade
-    
-# Criar menu principal do sistema e fazer a coleta dos dados
-from autenticador import titulo
-from autenticador import menu
-    
-escolha = 0
-while escolha != 3:
+while True:
     sleep(1)
     opcs = ['Verificar pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema']
     menu(opcs)
-    while True:
-        try:
-            escolha = int(input('\033[0;33mOpção desejada: \033[m'))
-            intervalo(escolha, 1, 4)
-        except NumeroForaDoIntervalo:
-            print('\033[0;31mOpção inválida! Digite um número entre 1 e 3...\033[m')
-        except:
-            print('\033[0;31mOpção inválida! Digite um número entre 1 e 3...\033[m')  
-        else:
-            break
-        
-    # Bloco que lista as pessoas cadastradas
-    if escolha == 1:
-        sleep(1)
-        titulo('PESSOAS CADASTRADAS')
-        with open("C://Users/guilh/Documents/GitHub/exercicios-curso-de-python/PythonExercises/Mundo 3 - modulos/exercicios/ex115/Cadastro.txt", "r") as arquivo:
-            ficha = arquivo.read()
-        print(ficha)
-        sleep(1)
-        
-    # Bloco que realiza um novo cadastro
-    if escolha == 2:
-        sleep(1)
-        titulo('NOVO CADASTRO')
-        nome = leiaNome('Nome: ')
-        idade = leiaIdade('Idade: ')
-        # Guarda as informações digitadas em um arquivo .txt; Caso não exista, será criado na hora
-        with open("C://Users/guilh/Documents/GitHub/exercicios-curso-de-python/PythonExercises/Mundo 3 - modulos/exercicios/ex115/Cadastro.txt", "a") as arquivo: # Arquivo == variável que recebe o .txt
-            arquivo.write(f'\n{nome.ljust(40)} {idade} anos')    
-        print(f'Novo registro de {nome} adicionado. ')
-        sleep(1)
-    
+    escolha = opçao()
+    if escolha in range(1, 3):
+        operaçao(escolha)
     # Bloco que exibe uma mensagem de despedida após o usuário sair 
     if escolha == 3:
         sleep(1)
         titulo('SAINDO DO SISTEMA... ATÉ MAIS!')
         sleep(1)
+        break
